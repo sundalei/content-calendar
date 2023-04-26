1742f1faaa2a674852f405975dcef17ef44d1ee1
Sun DaLei <sundalei1988@gmail.com>
2023-04-26 16:49:18 +0800

    spring jdbc with template repository

--- src/main/java/com/example/ContentCalendarApplication.java
+++ src/main/java/com/example/ContentCalendarApplication.java
@@ -1,11 +1,5 @@
 package com.example;
 
-import java.sql.Connection;
-import java.sql.DriverManager;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
-import java.sql.SQLException;
-
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
@@ -14,23 +8,6 @@ public class ContentCalendarApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ContentCalendarApplication.class, args);
-
-		final String jdbcURL = "jdbc:postgresql://localhost:5432/postgres";
-
-		try (Connection conn = DriverManager.getConnection(jdbcURL, "postgres", "password")) {
-
-			if (!conn.isValid(0)) {
-				System.out.println("Unable to connect to database");
-				System.exit(0);
-			}
-
-			PreparedStatement preparedStatement = conn.prepareStatement("SELECT * FROM Run");
-			ResultSet rs = preparedStatement.executeQuery();
-
-			
-		} catch (SQLException exception) {
-
-		}
 	}
 
 }
